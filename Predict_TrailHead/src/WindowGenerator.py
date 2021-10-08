40af265d36f7f5f7d2a568a4adec6ea373156ab3
Travis J Morrison <tjmorrison635@gmail.com>
2021-10-08 14:28:40 -0600

    update
    here is update

--- Predict_TrailHead/src/WindowGenerator.py
+++ Predict_TrailHead/src/WindowGenerator.py
@@ -1,7 +1,7 @@
 class WindowGenerator():
   def __init__(self, input_width, label_width, shift,
-              train_df=train_df, val_df=val_df,
-              test_df=test_df,label_columns=None):
+              train_df= train_df,val_df=val_df,test_df= test_df,
+              label_columns=None):
     # Store the raw data.
     self.train_df = train_df
     self.val_df = val_df
